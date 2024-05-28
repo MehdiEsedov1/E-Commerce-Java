9b93fed5900e6aa985d022afef8d09b4c0fd86ae
Məhdi Əsədov <mehdiesedof@gmail.com>
2024-05-28 10:19:18 +0400

    Add product service

--- src/main/java/com/example/ecommerce2/controller/ProductController.java
+++ src/main/java/com/example/ecommerce2/controller/ProductController.java
@@ -12,25 +12,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-//    private final ProductService productService;
-//
-//    @GetMapping("/getAllProducts")
-//    List<ProductResponse> getAllProducts(){
-//        return productService.getAllProducts();
-//    }
-//
-//    @PostMapping("/postProduct")
-//    void postProduct(@RequestBody ProductRequest productRequest){
-//        productService.postProduct(productRequest);
-//    }
-//
-//    @GetMapping("/getProductById/{id}")
-//    ProductResponse getProductById(@PathVariable Long id){
-//        return productService.getProductById(id);
-//    }
-//
-//    @PutMapping("/updateProduct")
-//    void updateProduct(@PathVariable Long id,@RequestBody ProductRequest productRequest){
-//        productService.updateProduct(id,productRequest);
-//    }
+    private final ProductService productService;
+
+    @GetMapping("/getAllProducts")
+    List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @PostMapping("/postProduct")
+    void postProduct(@RequestBody ProductRequest productRequest) {
+        productService.postProduct(productRequest);
+    }
+
+    @GetMapping("/getProductById/{id}")
+    ProductResponse getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PutMapping("/updateProduct")
+    void updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+        productService.updateProduct(id, productRequest);
+    }
 }
--- src/main/java/com/example/ecommerce2/service/impl/impl/ProductServiceImpl.java
+++ src/main/java/com/example/ecommerce2/service/impl/impl/ProductServiceImpl.java
@@ -1,7 +1,51 @@
 package com.example.ecommerce2.service.impl.impl;
 
+import com.example.ecommerce2.mapper.ProductMapper;
+import com.example.ecommerce2.model.dto.request.ProductRequest;
+import com.example.ecommerce2.model.dto.response.ProductResponse;
+import com.example.ecommerce2.model.entity.Product;
+import com.example.ecommerce2.repository.ProductRepo;
+import com.example.ecommerce2.service.impl.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-public class ProductServiceImpl {
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepo productRepo;
+    private final ProductMapper mapper;
+
+    @Override
+    public List<ProductResponse> getAllProducts() {
+        List<Product> productList = productRepo.findAll();
+        return productList.stream()
+                .map(product -> mapper.map(product))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void postProduct(ProductRequest productRequest) {
+        productRepo.save(mapper.map(productRequest));
+    }
+
+    @Override
+    public ProductResponse getProductById(Long id) {
+        return mapper.map(productRepo.findById(id).orElseThrow());
+    }
+
+    @Override
+    public void updateProduct(Long id, ProductRequest productRequest) {
+        Product product = productRepo.findById(id).orElseThrow();
+
+        product.setProductDetails(productRequest.getProductDetails());
+        product.setName(productRequest.getName());
+        product.setDescription(productRequest.getDescription());
+        product.setAmount(productRequest.getAmount());
+        product.setRemainCount(productRequest.getRemainCount());
+
+        productRepo.save(product);
+    }
 }
