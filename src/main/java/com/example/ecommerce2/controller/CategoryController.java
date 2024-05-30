0493992b0f477d72ff995dbb28de1fd5c7441ea9
Məhdi Əsədov <mehdiesedof@gmail.com>
2024-05-30 23:00:53 +0400

    Configure OpenAPI

--- src/main/java/com/example/ecommerce2/controller/CategoryController.java
+++ src/main/java/com/example/ecommerce2/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.example.ecommerce2.controller;
 import com.example.ecommerce2.model.dto.request.CategoryRequest;
 import com.example.ecommerce2.model.dto.response.CategoryResponse;
 import com.example.ecommerce2.service.impl.CategoryService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/category")
+@Tag(name = "Category Controller", description = "This is controller about categories")
 public class CategoryController {
     private final CategoryService categoryService;
 
--- src/main/java/com/example/ecommerce2/controller/ProductController.java
+++ src/main/java/com/example/ecommerce2/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.ecommerce2.controller;
 import com.example.ecommerce2.model.dto.request.ProductRequest;
 import com.example.ecommerce2.model.dto.response.ProductResponse;
 import com.example.ecommerce2.service.impl.ProductService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/product")
+@Tag(name = "Product Controller", description = "This is controller about products")
 public class ProductController {
     private final ProductService productService;
 
